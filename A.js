fea4c09741297b1df4bb43b6fa7855beb7ff99bf
killuaps <trjan40@gmail.com>
2017-11-15 14:14:29 +0200

    Add files via upload

---
+++ A.js
@@ -0,0 +1,12 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#cnmuprot')['html']('<a href="https://www.facebook.com/Abdallah.Mohamed80" rel="dofollow" target="_blank">Abdallah Mohamed</a>');
+        setInterval(function () {
+                if (!$('#cnmuprot:visible')['length']) {
+                  window['location']['href'] = 'https://www.facebook.com/Abdallah.Mohamed80';
+                };
+            }, 3000);
+    });
+//]]>
+</script>
